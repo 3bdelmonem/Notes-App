af7c0227ed15e33d1b9af4c9d668ecd28f47176b
Emadabdelhamied <emadabdelhamied83@gmail.com>
2023-06-03 02:58:16 +0300

    Notification

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/Crud/editNote.dart';
@@ -10,39 +12,37 @@ class Home extends StatefulWidget {
   final String email;
   final String password;
 
-
-  const Home({
-    required this.id,
-    required this.email,
-    required this.username,
-    required this.password,
-    super.key
-  });
+  const Home(
+      {required this.id,
+      required this.email,
+      required this.username,
+      required this.password,
+      super.key});
   @override
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {  
-  late CollectionReference notes = FirebaseFirestore.instance.collection("notes").doc(widget.id).collection("userNotes");
+class _HomeState extends State<Home> {
+  late CollectionReference notes = FirebaseFirestore.instance
+      .collection("notes")
+      .doc(widget.id)
+      .collection("userNotes");
 
-  void setupPushNotification() async{
+  void setupPushNotification() async {
     FirebaseMessaging fcm = FirebaseMessaging.instance;
-    await fcm.requestPermission(
-
-    );
+    await fcm.requestPermission();
     final token = await fcm.getToken();
-    print("================================");
-    print(token);
+    log("================================");
+    log(token.toString());
   }
 
   @override
   void initState() {
-
-  setupPushNotification();
+    setupPushNotification();
 
     FirebaseMessaging.onMessageOpenedApp.listen((event) {
       print("=========================================");
-      print("${event.notification!.body}");
+      log("${event.notification!.body}");
       print("=========================================");
       // Navigator.of(context).pushNamed("AddNote");
     });
@@ -62,11 +62,16 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         backgroundColor: Color(0xFF6034A6),
         foregroundColor: Colors.white,
-        title: Text(widget.username, style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700, color: Colors.white)),
+        title: Text(widget.username,
+            style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w700,
+                color: Colors.white)),
         centerTitle: true,
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
       ),
-      drawer: Mydrawer(widget.username, widget.email, widget.password, widget.id),
+      drawer:
+          Mydrawer(widget.username, widget.email, widget.password, widget.id),
       floatingActionButton: FloatingActionButton(
         onPressed: (() {
           Navigator.of(context).pushNamed("AddNote");
@@ -79,84 +84,104 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.all(20),
-        color: Color(0xFF0F0F1E),
-        child: StreamBuilder(
-          stream: notes.snapshots(),
-          builder: (context, snapshot) {
-            if(snapshot.hasData) {
-              return ListView.builder(
-                physics: BouncingScrollPhysics(),
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  return Dismissible(
-                    onDismissed: (direction) {
-                      notes.doc(snapshot.data!.docs[index]["noteId"]).delete().then((value) {
-                        print("Deleted Successful");
-                      }).catchError((e){
-                        print("Error = $e");
-                      });
-                    },
-                    direction: DismissDirection.endToStart,
-                    background: Container(
-                      alignment: Alignment.centerRight,
-                      margin: EdgeInsets.symmetric(vertical: 20),
-                      padding: EdgeInsets.only(right: 20),
-                      decoration: BoxDecoration(
-                          color: Colors.red, borderRadius: BorderRadius.circular(15)),
-                      child: Icon(Icons.delete, color: Colors.white, size: 35),
-                    ),
-                    key: Key(snapshot.data!.docs[index]["noteId"]),
-                    child: Container(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      decoration: BoxDecoration(
-                          gradient: RadialGradient(
-                            colors: [Color(0xFF6034A6), Color(0xFF4833A6)],
-                            radius: 2.5,
-                          ),
-                          borderRadius: BorderRadius.circular(15)),
-                      child: ListTile(
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => EditNote(
-                            noteId: snapshot.data!.docs[index]["noteId"],
-                            title: snapshot.data!.docs[index]["title"],
-                            content: snapshot.data!.docs[index]["content"],
-                          )));
-                        },
-                        contentPadding:
-                            EdgeInsets.fromLTRB(10, 5, 15, 5),
-                        leading: Icon(Icons.note, color: Color(0xFF0F0F1E), size: 50),
-                        title: Text(
-                          "${snapshot.data!.docs[index]["title"]}",
-                          style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold)),
-                        subtitle: SizedBox(
-                          width: 200,
-                          child: Text(
-                            "${snapshot.data!.docs[index]["content"]}",
-                            style: TextStyle(color: Color(0xFFAEAEB3), fontSize: 11),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 3,
-                          ),
-                        ),
-                        isThreeLine: true,
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(20),
+          color: Color(0xFF0F0F1E),
+          child: StreamBuilder(
+            stream: notes.snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    return Dismissible(
+                      onDismissed: (direction) {
+                        notes
+                            .doc(snapshot.data!.docs[index]["noteId"])
+                            .delete()
+                            .then((value) {
+                          print("Deleted Successful");
+                        }).catchError((e) {
+                          print("Error = $e");
+                        });
+                      },
+                      direction: DismissDirection.endToStart,
+                      background: Container(
+                        alignment: Alignment.centerRight,
+                        margin: EdgeInsets.symmetric(vertical: 20),
+                        padding: EdgeInsets.only(right: 20),
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(15)),
+                        child:
+                            Icon(Icons.delete, color: Colors.white, size: 35),
                       ),
-                    ),
-                  );
-                },
-              );
-            }
-            if(snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
-            if(snapshot.hasError) {
-            return Center(child: Text("Error.. Try Again Later", style: TextStyle(color: Colors.red, fontSize: 16, fontWeight: FontWeight.w700)));
-              
-            }
-            return Center(child: Text("Write It Down Before You Forget It", style: TextStyle(color: Colors.white54, fontSize: 16, fontWeight: FontWeight.w700)));
-          },
-        )
-      ),
+                      key: Key(snapshot.data!.docs[index]["noteId"]),
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                            gradient: RadialGradient(
+                              colors: [Color(0xFF6034A6), Color(0xFF4833A6)],
+                              radius: 2.5,
+                            ),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: ListTile(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => EditNote(
+                                      noteId: snapshot.data!.docs[index]
+                                          ["noteId"],
+                                      title: snapshot.data!.docs[index]
+                                          ["title"],
+                                      content: snapshot.data!.docs[index]
+                                          ["content"],
+                                    )));
+                          },
+                          contentPadding: EdgeInsets.fromLTRB(10, 5, 15, 5),
+                          leading: Icon(Icons.note,
+                              color: Color(0xFF0F0F1E), size: 50),
+                          title: Text("${snapshot.data!.docs[index]["title"]}",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold)),
+                          subtitle: SizedBox(
+                            width: 200,
+                            child: Text(
+                              "${snapshot.data!.docs[index]["content"]}",
+                              style: TextStyle(
+                                  color: Color(0xFFAEAEB3), fontSize: 11),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 3,
+                            ),
+                          ),
+                          isThreeLine: true,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return Center(
+                    child: Text("Error.. Try Again Later",
+                        style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700)));
+              }
+              return Center(
+                  child: Text("Write It Down Before You Forget It",
+                      style: TextStyle(
+                          color: Colors.white54,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700)));
+            },
+          )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:notes/Authentication/login.dart';
 import 'package:notes/Authentication/signUp.dart';
 import 'package:notes/Component/help.dart';
@@ -11,40 +11,31 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:notes/Authentication/splashScreen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'Home/notification_service.dart';
+
 late bool isActive;
 late String id;
 late String username;
 late String email;
 late String password;
 
-// Future bgMassage(RemoteMessage message) async{
-//   print("=========================================");
-//   print("${message.notification!.body}");
-//   print("=========================================");
-// }
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
-  FlutterLocalNotificationsPlugin();
-
-  
-
-  // FirebaseMessaging.onBackgroundMessage(bgMassage);
-
+  NotificationService.instance!.initNotificationService();
+  String? nooo = await NotificationService.instance!.getToken();
+  log(nooo.toString());
   final SharedPreferences prefs = await SharedPreferences.getInstance();
 
   var activeUser = FirebaseAuth.instance.currentUser;
-  if(activeUser == null){
+  if (activeUser == null) {
     isActive = false;
-  }
-  else{
+  } else {
     prefs.reload();
-    id = prefs.getString("id")??"";
-    username = prefs.getString("username")??"";
-    email = prefs.getString("email")??"";
-    password = prefs.getString("password")??"";
+    id = prefs.getString("id") ?? "";
+    username = prefs.getString("username") ?? "";
+    email = prefs.getString("email") ?? "";
+    password = prefs.getString("password") ?? "";
     isActive = true;
   }
   runApp(MyApp());
@@ -55,21 +46,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: FFStorage(),
-      home: isActive == true ? Home(id: id, username: username, email: email, password: password,) : splashScreen(),
+      home: isActive == true
+          ? Home(
+              id: id,
+              username: username,
+              email: email,
+              password: password,
+            )
+          : splashScreen(),
       debugShowCheckedModeBanner: false,
       routes: {
-        "Login":(context) =>  Login(),
-        "SignUp":(context) => SignUp(),
-        "AddNote":(context) => AddNote(),
-        "SplashScreen":(context) => splashScreen(),
-        "Help":(context) => HelpScreen(),
+        "Login": (context) => Login(),
+        "SignUp": (context) => SignUp(),
+        "AddNote": (context) => AddNote(),
+        "SplashScreen": (context) => splashScreen(),
+        "Help": (context) => HelpScreen(),
       },
       theme: ThemeData(
-        primaryColor: Color(0xFF6034A6),
-        textTheme: TextTheme(
-          labelLarge: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
-        )
-      ),
+          primaryColor: Color(0xFF6034A6),
+          textTheme: TextTheme(
+            labelLarge: TextStyle(
+                color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+          )),
     );
   }
 }
